3f037170fd35cb267c95a48c37032b9cbda77a30
David Quintanilla <david.quintanilla.eufragio@gmail.com>
2025-06-12 21:57:25 +0200

    feat: add minor chords

--- components/piano/ChordSelector.vue
+++ components/piano/ChordSelector.vue
@@ -1,86 +1,29 @@
 <template>
   <div class="h-70 flex gap-2 flex-col">
-    <p class="font-bold">
-      Acordes mayores - major / maj
-    </p>
-    <p>
-      Formula: 1 + 4 + 3
-    </p>
-    <UButtonGroup>
-      <UButton
-        v-for="chord in chordList"
-        :key="chord.id"
-        color="neutral"
-        size="xl"
-        variant="outline"
-        :label="chord.name"
-        class="w-15 h-10 flex justify-center"
-        @click="pianoStore.state.selectedChord = chord"
-      />
-    </UButtonGroup>
-    <p class="flex justify-center text-9xl font-extrabold">
-      {{ pianoStore.state.selectedChord?.name }}
-    </p>
+    <UTabs :items="items" class="w-full" color="neutral" variant="link">
+      <template #major-chords>
+        <PianoMajorChords />
+      </template>
+      <template #minor-chords>
+        <PianoMinorChords />
+      </template>
+    </UTabs>
   </div>
 </template>
 
 <script setup lang="ts">
-const pianoStore = usePianoStore()
-const i18n = useI18n()
+import type { TabsItem } from '@nuxt/ui'
 
-const chordList = [
+const items = ref<TabsItem[]>([
   {
-    id: 1,
-    name: i18n.t('note.c'),
-    octaves: {
-      2: { notes: [1, 5, 8] }
-    }
+    label: 'Acordes mayores',
+    icon: 'entypo:note',
+    slot: 'major-chords'
   },
   {
-    id: 2,
-    name: i18n.t('note.d'),
-    octaves: {
-      2: { notes: [3, 7, 10] }
-    }
-  },
-  {
-    id: 3,
-    name: i18n.t('note.e'),
-    octaves: {
-      2: { notes: [5, 9, 12] }
-    }
-  },
-  {
-    id: 4,
-    name: i18n.t('note.f'),
-    octaves: {
-      2: { notes: [6, 10] },
-      3: { notes: [1] }
-    }
-  },
-  {
-    id: 5,
-    name: i18n.t('note.g'),
-    octaves: {
-      2: { notes: [8, 12] },
-      3: { notes: [3] }
-    }
-  },
-  {
-    id: 6,
-    name: i18n.t('note.a'),
-    octaves: {
-      2: { notes: [10] },
-      3: { notes: [2, 5] }
-    }
-  },
-  {
-    id: 7,
-    name: i18n.t('note.b'),
-    octaves: {
-      2: { notes: [12] },
-      3: { notes: [4, 7] }
-    }
-  },
-]
+    label: 'Acordes menores',
+    icon: 'entypo:note',
+    slot: 'minor-chords'
+  }
+])
 </script>
