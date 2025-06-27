87471b1f1abffeb7e702a2dca5dd53b0a6aa4623
David Quintanilla <david.quintanilla.eufragio@gmail.com>
2025-06-27 21:13:21 +0200

    feat: add mixpanel

--- app.vue
+++ app.vue
@@ -5,6 +5,14 @@
 </template>
 
 <script setup lang="ts">
+import mixpanel from 'mixpanel-browser'
+
+mixpanel.init('05b7a75a246691059250585c8e3a2432')
+
+mixpanel.track('screen', {
+  name: 'pagina principal'
+})
+
 useHead({
   title: 'Pianight',
   htmlAttrs: {
