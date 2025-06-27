7c1d42691969d198026f8c03a2a955354a8ed71e
David Quintanilla <david.quintanilla.eufragio@gmail.com>
2025-06-27 16:27:54 +0200

    fix: update chord category

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-8 pt-4 flex gap-2">
-    <div class="flex justify-between items-start">
-      <PianoChordNavigator />
+    <div class="flex justify-between items-start gap-4">
       <PianoNoteSelector />
+      <PianoChordNavigator />
     </div>
     <PianoChordDetails class="w-full" />
   </div>
